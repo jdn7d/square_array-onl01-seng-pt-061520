6475f72c07dae5729fe8e9148b7e1f8bc919fd2b
Jessie Donovan <donovanajessie@gmail.com>
2020-06-23 01:23:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  
-  array.each do |number| 
-    new_number = number**2 
+
+  array.each do |number|
+    new_number = number**2
     new_numbers.push(new_number)
   end
   return new_numbers
